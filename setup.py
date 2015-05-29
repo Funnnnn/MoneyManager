3e7e0aa169c32ea218ec58dd6cca138d599eda0a
Ryan Hiebert <ryan@ryanhiebert.com>
2015-05-29 17:35:00 -0500

    Change the name to moneyme

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MoneyMe',
+    name='moneyme',
     version='0.0.0',
     description='Simple Money Management',
     author='Trevor Hiebert',
-    url='https://github.com/Funnnnn/MoneyManager',
+    url='https://github.com/Funnnnn/moneyme',
     packages=find_packages(exclude='tests'),
     entry_points={
         'console_scripts': [
